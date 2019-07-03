33ef5511b07ee1d3f3a6c3c969264a2a4878d837
Lesley <lesleydreyer@hotmail.com>
2019-07-03 12:59:45 -0700

    removed burger since not sure if wanted

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,8 @@
-$(document).ready(function() {
-    $(".js-btn").click(function(){
+$(document).ready(function () {
+    $(".js-btn").click(function () {
         alert("clicked");
-    }); 
-    $(".button").click(function(){
-        alert("clicked");
-    }); 
-    
-
-        /* Mobile Menu */
-    const menu = $(".mobile-nav-menu");
-
-    menu.addClass('fa-bars');//add the burger menu bars icon
-
-    menu.on('click', function () {
-        $('.js--main-nav').slideToggle(200);//toggle the menu links shown or hiddin
-        if (menu.hasClass('fa-bars')) {
-            menu.removeClass('fa-bars').addClass('fa-times');//replace bars with X
-        } else {
-            menu.removeClass('fa-times').addClass('fa-bars');//replace X with bars
-        }
     });
-    
+    $(".button").click(function () {
+        alert("clicked");
+    });
 });
